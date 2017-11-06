f7e83984cfe5ef0a229fad10257f64797d11b595
Fatima Burke <fatima@lurmarketing.com>
2017-11-06 14:53:58 -0500

    HTML structure edits and img files added

--- app.rb
+++ app.rb
@@ -1,14 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sendgrid-ruby'
+require 'pry'
 
-get '/' do
+include SendGrid
+
+get '/index' do
   erb :index
 end
 
-get '/' do
+get '/layout' do
   erb :layout
 end
 
-get '/' do
+get '/contact' do
   erb :contact
+end
+
+post '/' do
+  # binding.pry
+
+  from = Email.new(email: params['email'])
+  to = Email.new(email: 'test@example.com')
+  subject = 'Sending with SendGrid is Fun'
+  content = Content.new(type: 'text/plain', value: params['body'])
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+  erb:index
 end
